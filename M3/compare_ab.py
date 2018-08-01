7afe29dee235efa8f9c02c14bc6b6289be083612
pvsteja <tejapathri6@msitprogram.net>
2018-08-01 18:50:49 +0530

    v2

---
+++ M3/compare_ab.py
@@ -0,0 +1,10 @@
+"""Compare"""
+VARA = int(input("enter the varA value"))
+VARB = int(input("enter the varB value"))
+if VARA > VARB:
+    print("Bigger")
+if VARA < VARB:
+    print("smaller")
+if VARA == VARB:
+    print("equal")
+    	
