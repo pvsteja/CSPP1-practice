67a42ae8f9bba1a978fb48e90beef874d1ccfae4
pvsteja <tejapathri6@msitprogram.net>
2018-08-08 22:03:37 +0530

    v2

--- M9/Odd Tuples Exercise/odd_tuples.py
+++ M9/Odd Tuples Exercise/odd_tuples.py
@@ -1,26 +1,21 @@
-"""Exercise : Odd Tuples"""
-#Write a python function oddTuples(aTup) that takes a some numbers in the tuple as input.
-#returns a tuple in which contains odd index values in the input tuple.
-def odd_tuples(a_tup):
-    '''
-    a_tup: a tuple
-    returns: tuple, every other element of aTup.
-    '''
-    # Your Code Here
-    n_w = len(a_tup)
-    new_tuple = ()
-    i = 0
-    for i in range(n_w):
-        if i%2 != 0:
-            new_tuple = new_tuple + (a_tup[i],)
+#Exercise : Odd Tuples
+#Write a python function oddTuples(aTup) that takes a some numbers in the tuple as input and returns a tuple in which contains odd index values in the input tuple  
 
-    return new_tuple
+def oddTuples(aTup):
+    tup=()
+    for j in range(0, len(aTup), 2):
+        tup = tup +(aTup[j],)
+    return tup
+    
+
 def main():
-    """odd_tuples"""
     data = input()
     data = data.split()
-    a_tup = ()
-    for j in enumerate(data):
-        a_tup += (data[j],)
-    print(odd_tuples(a_tup))
-main()
+    aTup=()
+    for j in range(len(data)):
+        aTup += (int(data[j]),)
+    print(oddTuples(aTup))
+        
+
+if __name__ == "__main__":
+    main()
