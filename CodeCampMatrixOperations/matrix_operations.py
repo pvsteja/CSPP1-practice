62b015963e74c9c17bf8647cc7a4e897425a3f00
pvsteja <tejapathri6@msitprogram.net>
2018-08-23 15:37:46 +0530

    " -> 0 of 8 passed. pylint: 6.98/10 "

--- CodeCampMatrixOperations/matrix_operations.py
+++ CodeCampMatrixOperations/matrix_operations.py
@@ -15,12 +15,17 @@ def mult_matrix(m1, m2):
                 sum = 0
                 for k in range(len(m2)):
                     sum = sum + (m1[i][k]*m2[k][j])
+                    k += 1
                 a.append(sum)
+                j += 1
             b.append(a)
+            i += 1
             a = []
         return b
+    print("Error: Matrix shapes invalid for mult")
+    return None
 
-def add_matrix(M_1, M_2):
+def add_matrix(matrix_1, matrix_2):
     '''
         check if the matrix shapes are similar
         add the matrices and return the result matrix
@@ -29,18 +34,19 @@ def add_matrix(M_1, M_2):
         error message should be "Error: Matrix shapes invalid for addition"
     '''
     add_matrix = []
-    if len(M_1) == len(M_2):
-        for i in range(len(M_1)):
-            lst =[]
-            for j in range(len(M_1[0])):
-                lst.append(int(M_1[i][j]) + int(M_2[i][j]))
+    if len(matrix_1) == len(matrix_2):
+        for i in range(len(matrix_1)):
+            lst = []
+            for j in range(len(matrix_1[0])):
+                lst.append(int(matrix_1[i][j]) + int(matrix_2[i][j]))
+                j += 1
             add_matrix.append(lst)
+            i += 1
         return add_matrix
-    else:
-        print("Error: Matrix shapes invalid for addition")
-        return None     
+    print("Error: Matrix shapes invalid for addition")
+    return None
 
-def read_matrix(): 
+def read_matrix():
     '''
         read the matrix dimensions from input
         create a list of lists and read the numbers into it
@@ -57,29 +63,28 @@ def read_matrix():
         if len(line) == cols:
         # print(i, line)
         # lst = []
-        # for j in line: 
+        # for j in line:
         #     lst.append(int(j))
         # matrix.append(lst)
             matrix.append([int(j) for j in line])
-        else:
-            print("Error: Invalid input for the matrix")
-            return None
+            i += 1
+        print("Error: Invalid input for the matrix")
+        return None
     # print(rows, cols)
     # print(matrix)
     return matrix
-      
 
 def main():
     # read matrix 1
-    M_1 = read_matrix()
+    matrix_1 = read_matrix()
 
     # read matrix 2
-    M_2 = read_matrix()
+    matrix_2 = read_matrix()
 
     # add matrix 1 and matrix 2
-    if M_1 != None and M_2 != None:
-        print(add_matrix(M_1,M_2))
-        print(mult_matrix(M_1, M_2))
+    if matrix_1 != None and matrix_2 != None:
+        print(add_matrix(matrix_1, matrix_2))
+        print(mult_matrix(matrix_1, matrix_2))
 
     # multiply matrix 1 and matrix 2
 
