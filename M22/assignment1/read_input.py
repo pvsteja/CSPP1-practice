4a7fddabe763f689f9cb3f9577a12050a1be88b7
pvsteja <tejapathri6@msitprogram.net>
2018-08-25 12:24:45 +0530

    " -> 0 of 2 passed. pylint: 0.00/10 "

--- M22/assignment1/read_input.py
+++ M22/assignment1/read_input.py
@@ -3,7 +3,12 @@ Write a python program to read multiple lines of text input and store the input 
 '''
 
 def main():
-    pass
-
+	inp_1 =' '
+	line = int(input())
+	for i in range(line):
+		i += 1
+		inp_1 = input()
+		inp_1 += '\n'
+	print(input)	
 if __name__ == '__main__':
     main()
--- M22/assignment2/clean_input.py
+++ M22/assignment2/clean_input.py
@@ -4,10 +4,10 @@ alphabets in both upper and lower case and numbers.
 '''
 """clean string"""
 def clean_string(str):
-	for i in str:
+    for i in str:
         if i not in '!@#$%^&*()_+-=,.?1234567890':
             if i not in"'":
-"""removing special characters"""            	
+"""removing special characters"""
 def main():
     str = input()
     print(clean_string(str))
