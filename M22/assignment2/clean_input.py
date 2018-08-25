2a029ee176a9dfebc6d32f51193ec1f44057fed8
pvsteja <tejapathri6@msitprogram.net>
2018-08-25 13:20:40 +0530

    " -> 0 of 3 passed. pylint: 0.83/10 "

--- M22/assignment2/clean_input.py
+++ M22/assignment2/clean_input.py
@@ -1,9 +1,9 @@
 import re
 def clean_string(string):
-    inp_1 = ' '
+    input_1 = ' '
     reg = re.compile(["a-z, 0-9, A-Z"])
-    inp_1 = re.sub("", string.strip())
-    return inp_1
+    input_1 = re.sub("", string.strip())
+    return input_1
 """removing special characters"""
 def main():
     string = input()
